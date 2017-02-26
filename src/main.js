f236b286383349413f32df96baf5c654c37ee830
runshan <runshan.chen@implustech.com>
2017-02-26 15:26:18 +0800

    import radio group

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App'
-import {Button, Select, Input, Slider, Tooltip, Radio, Option} from 'element-ui'
+import {Button, Select, Input, Slider, Tooltip, Radio, RadioGroup, Option} from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
-[Button, Select, Input, Slider, Tooltip, Radio, Option].forEach(c => Vue.use(c))
+[Button, Select, Input, Slider, Tooltip, Radio, RadioGroup, Option].forEach(c => Vue.use(c))
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
